5ab45073072c0c527f4487062af7bf68afcb5a74
JoshKlein16 <joshuak4890@gmail.com>
2024-04-15 11:04:34 -0400

    added more to html

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 
-conn_str = "mysql://root:CSET@localhost/manage_banking"
+conn_str = "mysql://root:jedi4890@localhost/manage_banking"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
 @app.route('/')
 def start():
-    return render_template('index.html')
+    return render_template('base.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/adminLogin.html')
+def adminLogin():
+    return render_template('adminLogin.html')
 
 @app.route('/signup.html')
-def signup():
+def adminlogin():
     return render_template('signup.html')
-
-@app.route('/home.html')
-def student():
-    return render_template('studentHome.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
