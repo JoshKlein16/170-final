fa6c0f7b741245f41d4dfb0ccb0d331a6b3e2d55
flight04 <xshefcyk4633@stevenscollege.edu>
2024-04-15 11:05:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 
-conn_str = "mysql://root:Savier010523$@localhost/manage_banking"
+conn_str = "mysql://root:CSET@localhost/manage_banking"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
 @app.route('/')
 def start():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/home.html')
+def student():
+    return render_template('studentHome.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
