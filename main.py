3edbe9a4ef6214d8cd1736bc1930267ce9b6222c
JoshKlein16 <joshuak4890@gmail.com>
2024-04-18 23:04:31 -0400

    added python for everything

--- main.py
+++ main.py
@@ -9,66 +9,123 @@ conn = engine.connect()
 
 @app.route('/')
 def start():
-    return render_template('base.html')
+    return render_template('index.html')
 
-@app.route('/login.html')
-def login():
-    return render_template('login.html')
+@app.route('/createA.html')
+def signup():
+    return render_template('createA.html')
 
-@app.route('/adminLogin.html')
-def adminLogin():
-    return render_template('adminLogin.html')
+@app.route('/createA.html', methods=["GET"])
+def signupT():
+    return render_template("createA.html")
 
-@app.route('/signup.html')
-def adminlogin():
-    return render_template('signup.html')
+@app.route('/createA.html', methods=["POST"])
+def createArequest():
+    conn.execute(text("INSERT INTO request_Cuser (First_Name, Last_Name, Email, Password, Phone_Number, SSN, checking) VALUES (:fName, :lName, :email, :password, :phone, :ssn, '0')"),  request.form)
+    conn.commit()
+    return render_template("index.html")
 
-@app.route('/adminLogin.html', methods=["POST"])
+@app.route('/home.html')
+def student():
+    return render_template('index.html')
+
+
+
+@app.route('/pendingAccounts.html')
+def accounts():
+    query = text("SELECT Cuser_ID, First_Name, Last_Name, Email, Phone_Number, SSN FROM request_Cuser")
+    CuserData = conn.execute(query)
+
+    return render_template('pendingAccounts.html', CuserData=CuserData)
+
+
+
+
+@app.route('/bankerlogin.html', methods=["GET"])
+def loginS():
+    return render_template('bankerlogin.html')
+
+@app.route('/bankerlogin.html', methods=["POST"])
 def loginSGo():
     email = request.form['Email']
     password = request.form['Password']
-    query = text("SELECT bank_account_id FROM banker WHERE Email = :email AND Password = :password")
+    query = text("SELECT Cuser_ID FROM banker WHERE Email = :email AND Password = :password")
     user = conn.execute(query, {'email': email, 'password': password}).fetchone()
     if user:
         global BankID
         BankID = user[0]
-        query = text("SELECT First_Name FROM banker WHERE bank_account_id = :bank_account_id")
-        name = conn.execute(query, {'BankID' : BankID}).fetchone()
-        return render_template('adminView.html', name=name[0])
+        query = text("SELECT First_Name, FROM banker WHERE Cuser_ID = :Cuser_ID")
+        name = conn.execute(query, {'Cuser_ID' : BankID}).fetchone()
+        return render_template('adminpanel.html', name=name[0])
     else:
-        return render_template('adminLogin.html')
+        return render_template('bankerlogin.html')
+    
+@app.route('/login.html', methods=["GET"])
+def login():
+    return render_template('login.html')
 
-@app.route('/transfer.html')
-def transferPage():
+@app.route('/login.html', methods=["POST"])
+def loginGo():
+    email = request.form['Email']
+    password = request.form['Password']
+    query = text("SELECT Cuser_ID FROM Cuser WHERE Email = :email AND Password = :password")
+    user = conn.execute(query, {'email': email, 'password': password}).fetchone()
+    if user:
+        global BankID
+        BankID = user[0]
+        query = text("SELECT First_Name FROM Cuser WHERE Cuser_ID = :Cuser_ID")
+        name = conn.execute(query, {'Cuser_ID': BankID}).fetchone()
+        # GetInfo = text("SELECT First_Name, Last_Name, Email, Password, Phone_Number, SSN, checking FROM banker WHERE Cuser_ID = :Cuser_ID")
+        # accountInfo = conn.execute(GetInfo, {'Cuser_ID': BankID}).fetchone()
+        if name:
+            return render_template('home.html', name=name[0])
+        else:
+            return render_template('home.html', name="WHO DF ARE YOU")
+    else:
+        return render_template('index.html')
+    
+
+@app.route('/ViewAccount.html')
+def ViewAccount():
+    query = text("SELECT First_Name, Last_Name, Email, Password, Phone_Number, SSN, checking FROM Cuser WHERE Cuser_ID = :Cuser_ID")
+    accountInfo = conn.execute(query, {'Cuser_ID': BankID}).fetchone()
+
+    return render_template('ViewAccount.html', AccountData=accountInfo)
+
+@app.route('/transfer.html', methods=["GET"])
+def transfer():
     return render_template('transfer.html')
 
-@app.route('/transfer', methods=['POST'])
-def transfer():
-    sender_account_number = int(request.form['sender_account_number'])
-    receiver_account_number = int(request.form['receiver_account_number'])
+@app.route('/transfer', methods=["POST"])
+def transfer_money():
+    recipient_account_id = request.form['recipient_account_id']
     amount = float(request.form['amount'])
 
-    # Validate the sender and receiver account numbers
-    sender_query = text("SELECT * FROM users WHERE bank_account_id = :account_number")
-    sender = conn.execute(sender_query, {'account_number': sender_account_number}).fetchone()
-    receiver_query = text("SELECT * FROM users WHERE bank_account_id = :account_number")
-    receiver = conn.execute(receiver_query, {'account_number': receiver_account_number}).fetchone()
+    recipient_query = text("SELECT * FROM users WHERE bank_account_id = :recipient_account_id")
+    recipient = conn.execute(recipient_query, {'recipient_account_id': recipient_account_id}).fetchone()
 
-    if not sender or not receiver:
-        return 'Invalid account number'
+    if recipient:
+        global BankID
+        BankID = recipient[0]
+        user_query = text("SELECT balance FROM users WHERE bank_account_id = :user_account_id")
+        user_balance = conn.execute(user_query, {'user_account_id': BankID}).fetchone()[0]
 
-    # Check if the sender has sufficient balance for the transaction
-    if sender['balance'] < amount:
-        return 'Insufficient balance'
+        if user_balance >= amount:
+            new_user_balance = user_balance - amount
 
-    # Debit the sender's account and credit the receiver's account
-    debit_query = text("UPDATE users SET balance = balance - :amount WHERE bank_account_id = :account_number")
-    conn.execute(debit_query, {'amount': amount, 'account_number': sender_account_number})
-    credit_query = text("UPDATE users SET balance = balance + :amount WHERE bank_account_id = :account_number")
-    conn.execute(credit_query, {'amount': amount, 'account_number': receiver_account_number})
-    conn.commit()
+            update_user_query = text("UPDATE users SET balance = :new_balance WHERE bank_account_id = :user_account_id")
+            conn.execute(update_user_query, {'new_balance': new_user_balance, 'user_account_id': BankID})
 
-    return 'Transfer successful'
+            new_recipient_balance = recipient[1] + amount
+
+            update_recipient_query = text("UPDATE users SET balance = :new_balance WHERE bank_account_id = :recipient_account_id")
+            conn.execute(update_recipient_query, {'new_balance': new_recipient_balance, 'recipient_account_id': recipient_account_id})
+
+            return "Money transferred successfully."
+        else:
+            return "Insufficient balance."
+    else:
+        return "Recipient account not found."
 
 if __name__ == '__main__':
     app.run(debug=True)
